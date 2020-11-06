497e3a0ee7ea594e8486b6b0d694dc04ea0fcc78
Pablo Marcano <pablo.marcano.16@gmail.com>
2020-11-06 16:38:25 -0400

    asdfasdfasdfa

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@
 	$message = strip_tags(htmlspecialchars($_POST['message']));
 	   
 	// Create the email and send the message
-	$to = 'pablo.marcano.16@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+	$to = 'islapixel@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 	$email_subject = "Contaco desde su sitio web:  $name";
 	$email_body = "Se ha recibido un mensaje desde su sitio web.\n\n"."Aca estan los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTelefono: $phone\n\nMensaje:\n$message";
 	$headers = "De: noreply@islapixel.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
